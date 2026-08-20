df1b2208a68cfa251d24f923ceea929e4b4ef6c6
va1shakh <vaishakhvgd@gmail.com>
2026-08-20 14:00:51 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
    base: "/auth-system/",
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
 })
